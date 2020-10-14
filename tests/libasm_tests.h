eac49d9cbacb06ee517a902cb5b4328bfcb2cc62
ouram <mboivin@student.42.fr>
2020-10-14 17:12:57 +0200

    Split header

--- tests/libasm_tests.h
+++ tests/libasm_tests.h
@@ -6,15 +6,15 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 00:12:45 by mboivin           #+#    #+#             */
-/*   Updated: 2020/10/14 16:59:09 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/10/14 17:11:35 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBASM_H
-# define LIBASM_H
+#ifndef LIBASM_TESTS_H
+# define LIBASM_TESTS_H
 
-# define TEST_STR_00 "Hello World!\n"
 # define TEST_STR_EMPTY ""
+# define TEST_STR_00 "Hello World!\n"
 
 void	test_ft_write(void);
 void	test_ft_strlen(void);

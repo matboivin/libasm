c4645750226a4c1f10a1e9499e3fa5594cd007e2
ouram <mboivin@student.42.fr>
2020-10-22 01:58:14 +0200

    Add list size

---
+++ src/linux/ft_list_size.s
@@ -0,0 +1,25 @@
+%include "src/linux/libasm.s"
+
+        section .text
+        global  ft_list_size
+
+ft_list_size:
+        push    rbp                     ; enter
+        mov     rbp, rsp
+
+        xor     rax, rax                ; clears rax
+        xor     rcx, rcx                ; clears rcx (count)
+
+while_node:
+        cmp     BYTE [rdi], 0           ; checks for null data
+        je      done
+        inc     rcx                     ; increments count by 1
+        mov     rdi, [rdi + NODE_SIZE]  ; moves to next node
+        jmp     while_node
+
+done:
+        mov     rax, rcx                ; moves count in return
+
+        mov     rsp, rbp                ; leave
+        pop     rbp
+        ret
--- src/linux/libasm.s
+++ src/linux/libasm.s
@@ -1,3 +1,4 @@
 %define ERROR_VAL -1
 %define READ 0
 %define WRITE 1
+%define NODE_SIZE 8

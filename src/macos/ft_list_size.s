879f238bd6eaaac55bbb9deed604b84279887a1a
ouram <mboivin@student.42.fr>
2020-10-23 19:10:34 +0200

    Add macos ft_list_size version

---
+++ src/macos/ft_list_size.s
@@ -0,0 +1,25 @@
+%include "src/linux/libasm.s"
+
+        section .text
+        global  _ft_list_size
+
+_ft_list_size:
+        push    rbp                     ; enter
+        mov     rbp, rsp
+
+        xor     rax, rax                ; clears rax
+        xor     rcx, rcx                ; clears rcx (count)
+
+_while_node:
+        cmp     rdi, 0                  ; checks if null
+        je      _done
+        inc     rcx                     ; increments count by 1
+        mov     rdi, [rdi + NODE_SIZE]  ; moves to next node
+        jmp     _while_node
+
+_done:
+        mov     rax, rcx                ; moves count in return
+
+        mov     rsp, rbp                ; leave
+        pop     rbp
+        ret

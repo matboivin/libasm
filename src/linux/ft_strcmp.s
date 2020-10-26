7a69a857b6c02cce1e82176d92ad4e54bb908a56
ouram <mboivin@student.42.fr>
2020-10-26 17:04:35 +0100

    Fix strcmp

--- src/linux/ft_strcmp.s
+++ src/linux/ft_strcmp.s
@@ -8,30 +8,35 @@ ft_strcmp:
         push    rbp             ; enter
         mov     rbp, rsp
 
+        xor     rax, rax        ; clears rax
         xor     rdx, rdx        ; clears rdx
 
 loop_cmp:
-        xor     rax, rax        ; clears rax
-        cmp     BYTE [rdi], 0   ; checks for trailing null char in s1
-        je      done
-        cmp     BYTE [rsi], 0   ; checks for trailing null char in s1
-        je      done
         mov     al, BYTE [rdi]  ; moves s1 char in al
         mov     dl, BYTE [rsi]  ; moves s2 char in dl
+        cmp     BYTE [rdi], 0   ; checks for trailing null char in s1
+        je      endif
+        cmp     BYTE [rsi], 0   ; checks for trailing null char in s1
+        je      endif
         inc     rdi             ; increments s1
         inc     rsi             ; increments s2
         cmp     al, dl          ; checks if characters are equal
         je      loop_cmp
-        jl      s1_lesser
-        jg      s1_greater
 
-s1_lesser:
-        mov     rax, IS_LESS    ; s1 is less than s2
+endif:
+        cmp     al, dl          ; checks if characters are equal
+        jne     if_lesser
+        sub     rax, rdx        ; returns 0
         jmp     done
 
-s1_greater:
+if_lesser:
+        cmp     al, dl          ; checks if s1 char is lesser than s2 char
+        jnbe    if_greater
+        mov     rax, IS_LESS    ; s1 is lesser than s2
+        jmp     done
+
+if_greater:
         mov     rax, IS_GREATER ; s1 is greater than s2
-        jmp     done
 
 done:
         mov     rsp, rbp        ; leave
--- src/macos/ft_strcmp.s
+++ src/macos/ft_strcmp.s
@@ -8,30 +8,35 @@ _ft_strcmp:
         push    rbp             ; enter
         mov     rbp, rsp
 
+        xor     rax, rax        ; clears rax
         xor     rdx, rdx        ; clears rdx
 
 _loop_cmp:
-        xor     rax, rax        ; clears rax
-        cmp     BYTE [rdi], 0   ; checks for trailing null char in s1
-        je      _done
-        cmp     BYTE [rsi], 0   ; checks for trailing null char in s1
-        je      _done
         mov     al, BYTE [rdi]  ; moves s1 char in al
         mov     dl, BYTE [rsi]  ; moves s2 char in dl
+        cmp     BYTE [rdi], 0   ; checks for trailing null char in s1
+        je      _endif
+        cmp     BYTE [rsi], 0   ; checks for trailing null char in s1
+        je      _endif
         inc     rdi             ; increments s1
         inc     rsi             ; increments s2
         cmp     al, dl          ; checks if characters are equal
         je      _loop_cmp
-        jl      _s1_lesser
-        jg      _s1_greater
 
-_s1_lesser:
-        mov     rax, IS_LESS    ; s1 is less than s2
+_endif:
+        cmp     al, dl          ; checks if characters are equal
+        jne     _if_lesser
+        sub     rax, rdx        ; returns 0
         jmp     _done
 
-_s1_greater:
+_if_lesser:
+        cmp     al, dl          ; checks if s1 char is lesser than s2 char
+        jnbe    _if_greater
+        mov     rax, IS_LESS    ; s1 is lesser than s2
+        jmp     _done
+
+_if_greater:
         mov     rax, IS_GREATER ; s1 is greater than s2
-        jmp     _done
 
 _done:
         mov     rsp, rbp        ; leave
